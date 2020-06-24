6c2dcbecf2de9789143957232985e9b5ce4644ac
戴均民 <taichunmin@gmail.com>
2020-06-24 12:07:24 +0800

    Update 136.cpp

--- AC/136.cpp
+++ AC/136.cpp
@@ -1,39 +1,15 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    long long aa[1500];
-    int nowbig,cc,c2=3,c3=2,c5=1;
-    long long n2,n3,n5;
-    for(int i=0;i<6;i++)aa[i]=i+1;
-    for(int count=6;count<1500;count++)
-    {
-      nowbig=2147483647; 
-      cc=c2;
-      if(cc>c3)cc=c3;
-      if(cc>c5)cc=c5;
-      for(int k=cc;k<count;k++)
-      {
-        n2=2*aa[k]; n3=3*aa[k]; n5=5*aa[k];
-      	if (n2>aa[count-1] && n2<nowbig) 
-	       {
-	         nowbig=n2;
-	         c2=k;
-	       }
-      	else if (n3>aa[count-1] && n3<nowbig)
-      	{
-	       nowbig=n3;
-	       c3=k;
-	       }
-	       else if (n5>aa[count-1] && n5<nowbig)
-	       {
-	       nowbig=n5;
-	       c5=k;
-	       }
-      }
-      aa[count]=nowbig;
-    }
-    cout<<"The 1500'th ugly number is "<<aa[1499]<<"."<<endl;    
-    system("pause");
+int main() {
+  set<int> ugly;
+  ugly.insert(1);
+  for (int i=0; i<1499; i++) {
+    auto tmp = ugly.lower_bound(0);
+    ugly.insert(*tmp*2);
+    ugly.insert(*tmp*3);
+    ugly.insert(*tmp*5);
+    ugly.erase(tmp);
+  }
+  cout << "The 1500'th ugly number is " << *ugly.lower_bound(0) << "." << endl;
 }
